d657d4b7114519ec7d7782f20a2799ee046aa42a
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-08-23 20:11:45 +0300

    Exercise 1.7 done
    Unicafe step 2 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -5,6 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(allClicks + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(allClicks + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(allClicks + 1)
+  }
 
   return (
     <>
@@ -12,15 +28,18 @@ const App = () => {
         <h1>
           give feedback
         </h1>
-        <button onClick={() => setGood(good + 1)}> good </button>
-        <button onClick={() => setNeutral(neutral + 1)}> neutral </button>
-        <button onClick={() => setBad(bad + 1)}> bad </button>
+        <button onClick={handleGoodClick}> good </button>
+        <button onClick={handleNeutralClick}> neutral </button>
+        <button onClick={handleBadClick}> bad </button>
         <h1>
           statistics
         </h1>
         good {good} <br></br>
         neutral {neutral} <br></br>
-        bad {bad}
+        bad {bad} <br></br>
+        all {allClicks} <br></br>
+        average {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)} <br></br>
+        positive {good / allClicks * 100} %
       </div>
     </>
   )
