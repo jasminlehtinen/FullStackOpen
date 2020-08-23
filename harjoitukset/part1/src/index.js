2114e43399fc19e19f27ff0c856b96876313135a
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-08-23 19:18:19 +0300

    Exercise 1.6 done
    Unicafe step 1 done

--- harjoitukset/part1/src/index.js
+++ harjoitukset/part1/src/index.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const History = (props) => {
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+
+// HARJOITUS LEFT&RIGHT BUTTONS
+/*const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
       <div>
@@ -54,9 +85,9 @@ const App = (props) => {
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-)
+)*/
 
-
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
 // HARJOITUS COUNTER
 /*const Display = ({ counter }) => <div>{counter}</div>
@@ -66,7 +97,6 @@ const Button = ({ handleClick, text }) => (
     {text}
   </button>
 )
-
 
 const App = (props) => {
   const [ counter, setCounter ] = useState(0)
