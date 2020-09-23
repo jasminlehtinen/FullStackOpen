2c5b819799e2642938d41a293b1a0d033d4da7b3
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-09-23 13:49:25 +0300

    Exercise 2.6 done
    Puhelinluettelo, step 1

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,18 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ])
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
     </>
   )
 }
