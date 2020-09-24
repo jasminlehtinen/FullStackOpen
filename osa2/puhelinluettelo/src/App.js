9dc90d6a2d28fc3216c6da19202390123f066701
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-09-24 15:24:37 +0300

    Exercise 2.9* done
    Puhelinluettelo, step 4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas', number: '040-1231244' } ])
+  const [persons, setPersons] = useState([ 
+    { name: 'Ada Lovelace', number: '040-1234567' },
+    { name: 'Margaret Hamilton', number: '040-9876543' },
+    { name: 'Grace Hopper', number: '050-1234567' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')  
+  const [search, setSearch] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,19 +40,31 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchName = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const searchPhonebook = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <>
       <h2>Phonebook</h2>
+      <div>filter: <input value={search} onChange={handleSearchName}/></div>
+
+      <h2>Add a new contact</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNewName}/></div>
         <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
         <div><button type="submit">add</button></div>
       </form>
+
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <p key={person.name}>{person.name} {person.number}</p>
-        ))}
+        <ul>
+          {searchPhonebook.map(person => 
+            <li key={person.name}>{person.name} {person.number}</li>
+          )}
+        </ul>
       </div>
     </>
   )
