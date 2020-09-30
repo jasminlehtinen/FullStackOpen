6f60af6caf9bb72f5d4b7a43f29b0526db44d927
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-09-30 19:27:36 +0300

    Exercise 2.12* done
    Maiden tiedot, step 1

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = ({ search, handleSearch }) => {
+  return (
+    <div>Find countries: <input value={search} onChange={handleSearch} /></div>
+  )
+}
+
+const Countries = ({ countries, search }) => {
+
+  const searchForCountries = countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase())) 
+
+  if (searchForCountries.length === 0) {
+    return (
+      <div>
+        <p>No countries by that name!</p>
+      </div>
+    )
+  } else if (searchForCountries.length === 1) {
+    return (
+      <Country country = {searchForCountries[0]} />
+    )
+  } else if (searchForCountries.length < 10) {
+    return (
+      <div>
+        {searchForCountries.map(country => 
+          <p key={country.name}>{country.name}</p>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )
+  }
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>Capital {country.capital}</p>
+      <p>Population {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(language => 
+          <li key={language.name}>{language.name}</li>
+        )}
+      </ul>
+      <img src={country.flag} alt='Country flag' width='10%'></img>
+    </div>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  })
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  return (
+    <>
+      <Filter search={search} handleSearch={handleSearch} />
+      <Countries countries={countries} search={search} />
+    </>
+  )
+}
+
+export default App
+
