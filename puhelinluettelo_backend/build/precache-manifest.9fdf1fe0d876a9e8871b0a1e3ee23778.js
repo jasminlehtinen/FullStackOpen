308a6d59c5936889f0a851184d0b803fd11f74b1
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-10-28 15:20:25 +0200

    Exercise 3.11 done
    Puhelinluettelo full stack

---
+++ puhelinluettelo_backend/build/precache-manifest.9fdf1fe0d876a9e8871b0a1e3ee23778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee169cfd40e8305487fe3b02eae375d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6cd51162be9e63e5c1",
+    "url": "/static/css/main.8d23766e.chunk.css"
+  },
+  {
+    "revision": "1a15deb88bce7379561a",
+    "url": "/static/js/2.dbfacc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbfacc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6cd51162be9e63e5c1",
+    "url": "/static/js/main.319d0572.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
