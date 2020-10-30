484630992a99e16e13a0df925844038bd4c03c9d
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-10-30 20:08:51 +0200

    Exercise 3.16 done
    Puhelinluettelo ja tietokanta, step 4

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -6,49 +6,18 @@ const Contact = require('./models/contact')
 const { request, response } = require('express')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
-
-app.get('/', (req, res) => {
-    res.send('<h2>Phonebook</h2>')
-})
-
-app.get('/info', (req, res) => {
-    const date = new Date()
-    res.write(`<p>Phonebook has info for ${contacts.length} people</p>`)
-    res.write(`${date}`)
-    res.end()
-})
 
 app.get('/api/persons', (req, res) => {
     Contact.find({}).then(contacts => {
         res.json(contacts)
     })
 })
-
-app.get('/api/persons/:id', (request, response) => {
-    Contact.findById(request.params.id)
-        .then(contact => {
-            if (contact) {
-                response.json(contact)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'Malformatted id' })
-        })
-})
-
-const generateId = () => {
-    const id = Math.floor(Math.random() * 500)
-    return id
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -70,7 +39,6 @@ app.post('/api/persons', (request, response) => {
     const contact = new Contact({
         name: body.name,
         number: body.number,
-        id: generateId(),
     })
 
     contact.save().then(savedContact => {
@@ -78,12 +46,42 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Contact.findById(request.params.id)
+        .then(contact => {
+            if (contact) {
+                response.json(contact)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
     Contact.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'Malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
