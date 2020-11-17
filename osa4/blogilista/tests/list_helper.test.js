e264a4c63bba0de355cb85c6c9cf3a19d53c6369
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-11-17 18:56:10 +0200

    Exercise 4.4 done
    Apufunktioita ja yksikkötestejä, step 2

---
+++ osa4/blogilista/tests/list_helper.test.js
@@ -0,0 +1,67 @@
+const dummy = require('../utils/list_helper').dummy
+const totalLikes = require('../utils/list_helper').totalLikes
+
+test('Dummy returns one', () => {
+  const blogs = []
+
+  const result = dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('Total likes', () => {
+
+  const listWithNoBlogs = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5fb024cde5de4c0ad0e14828',
+      title: 'Advanced Mathematics',
+      author: 'Ada Lovelace',
+      url: 'http://www.blogs.com/ada-lovelace/advanced_mathematics',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      _id: '5fb024cde5de4c0ad0e14828',
+      title: 'Advanced Mathematics',
+      author: 'Ada Lovelace',
+      url: 'http://www.blogs.com/ada-lovelace/advanced_mathematics',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5fb026a2e5de4c0ad0e14829',
+      title: 'Architecture',
+      author: 'Kristen McIntyre',
+      url: 'http://www.blogs.com/coding101/kristen-mcintyre/architecture',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5fb2cb7bd9398c427c719577',
+      title: 'About Apollo 11',
+      author: 'Margaret Hamilton',
+      url: 'http://www.blogs.com/margaret-hamilton/about_apollo_11',
+      likes: 7,
+      __v: 0
+    } 
+  ]
+
+  test('When the list is empty, equal to 0', () => {
+    const result = totalLikes(listWithNoBlogs)
+    expect(result).toBe(0)
+  })
+
+  test('When the list has only one blog, all the likes of that blog', () => {
+    const result = totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('When the list has many blogs, sum of all the likes of the blogs', () => {
+    const result = totalLikes(listWithManyBlogs)
+    expect(result).toBe(22)
+  })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, current) => {
+    return sum + current.likes
+  }
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
