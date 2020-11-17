9845d8c59c57f4a46d170fcf72d99e96527b01cb
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-11-17 19:33:24 +0200

    Exercise 4.5* done
    Apufunktioita ja yksikkötestejä, step 3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,6 +11,25 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else {
+    let max = blogs[0]
+
+    blogs.forEach(blog => {
+      if (blog.likes > max.likes) {
+        max = blog
+      }
+    })
+    return {
+      title: max.title,
+      author: max.author,
+      likes: max.likes
+    }
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
