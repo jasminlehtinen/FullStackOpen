1e97b0617630f609cd7106f1c7eed46e299d1cc7
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2020-11-17 17:26:15 +0200

    Exercise 4.3 done
    Apufunktioita ja yksikkötestejä, step 1

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -10,12 +10,12 @@ const mongoose = require('mongoose')
 logger.info('Connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        logger.info('Connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('Error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
